83f8d655106540497e770c45411e8da6e5ee58e8
Dehghan <m.dehghan@navaco-co.local>
2020-12-01 16:03:13 +0330

    6.1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import {Switch,Route} from 'react-router-dom';
 import './App.css';
- import HomePage from './pages/homepage/homepage.component'
+ import HomePage from './pages/homepage/homepage.component';
+ import ShopPage from './pages/shop/shop-component';
 
 const HatsPage = () =>(
   <div>
@@ -15,7 +16,7 @@ function App() {
       
          <Switch>
            <Route exact path ='/' component={HomePage}/>
-           <Route exact path ='/hats' component={HatsPage}/>
+           <Route exact path ='/shop' component={ShopPage}/>
          </Switch>
     </div>
     
